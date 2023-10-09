9cb353e308a80c18a6748165a8e2c59cef709bbd
Marcos.P <marcos.p.mcruz@gmail.com>
2023-10-09 17:21:55 -0300

    Adding script to populate the database

---
+++ script/create.sql
@@ -0,0 +1,6 @@
+use fullcycle;
+
+create table people (
+  id int not null auto_increment, 
+  name varchar(255), 
+  primary key(id));
---
+++ script/insertPeople.sql
@@ -0,0 +1,4 @@
+use fullcycle;
+
+insert into people ( name )
+ values ('Teresa'), ('Maria'), ('Elias'), ('Messias'), ('Diana'), ('Marcos');
